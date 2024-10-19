6c6f824f683a8504476194e2dcbe9c65d702d52a
Sean <moriorpandemic@gmail.com>
2024-10-19 14:01:10 +0500

    preload all pages if navigation exist on page, or preload only next page

--- main.js
+++ main.js
@@ -16,6 +16,21 @@ try {
 	}
 
 
+	// TODO: track current page and load next page if rapidPreLoadAllPages is false (means load only next page not all from navigation)
+
+	// prealod next page
+	const rapidPreLoadAllPages = true;
+	let links = [...document.querySelectorAll('a[href*="page-"], a[href*="?page="]')]; // Select all links containing "page-"
+
+
+	if(links.length > 0){
+		if(!rapidPreLoadAllPages){
+			links = keepFirstElement(links);
+		}
+
+		links.map(link => fetchAndCacheHTMLImagesList(link.href).then(async images => handleImagesPreloading(images)));
+	}
+
 } catch (error) {
 	console.error('CANNOT SOME PRELOAD:', error);
 }
